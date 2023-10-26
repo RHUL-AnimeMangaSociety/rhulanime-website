f4fd1bf8ec92a0c2fd56ecbd0826e0ff6e98eabf
katsofroniou <katerina.sof@btinternet.com>
2023-10-26 04:34:13 +0100

    fix: added container to stop footer covering content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import '../styles/Footer.css'
 
 function Footer() {
     return (
-        <footer>
-            <p>Created by <a href={'https://github.com/katsofroniou'}>Katerina</a></p>
-        </footer>
+        <div className={'footer-container'}>
+            <footer>
+                <p>Created by <a href={'https://github.com/katsofroniou'}>Katerina</a></p>
+            </footer>
+        </div>
     )
 }
 
