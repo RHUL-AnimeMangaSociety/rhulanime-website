c4382772decaca66c72b69e1c0eb4bc0c291c367
katsofroniou <katerina.sof@btinternet.com>
2023-10-26 02:46:35 +0100

    feat: added mobile styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/Header.css"
 
 const links = [
-    { id: 1, name: "Home", pointer: "/#" },
-    { id: 2, name: "Join", pointer: "/#join" },
-    { id: 3, name: "Events", pointer: "/#events" },
-    { id: 4, name: "Committee", pointer: "/#committee" },
+    { id: 1, name: "Home", pointer: "#" },
+    { id: 2, name: "Join", pointer: "#join" },
+    { id: 3, name: "Events", pointer: "#events" },
+    { id: 4, name: "Committee", pointer: "#committee" },
 ];
 
 function Header() {
-    return (
-        <header className={"navbar"}>
-            <img src={"/colossus.png"} alt={"Anime & Manga Society Logo with Colossus-chan our mascot"} height={100}/>
+    const [active, setActive] = useState(false);
 
-            <ul className={'menu-links'}>
-            {links.map((link) =>
-                <li className={'header-menu-item'}><a href={link.pointer}>{link.name}</a></li>
-            )}
-            </ul>
+    const toggleMenu = () => {
+        setActive(!active);
+    };
+
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <img
+                    className="colossus"
+                    src="/colossus.png"
+                    alt="Colossus-chan"
+                    height={100}
+                />
+
+                <div className={`hamburger ${active ? "active" : ""}`} onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+
+                <img
+                    className={'mobile-logo'}
+                    src={"/logo2023.png"}
+                    alt={"Anime & Manga Society Logo"}
+                    height={'100px'}
+                />
+
+                <ul className={`menu-links ${active ? "active" : ""}`}>
+                    {links.map((link) => (
+                        <li className="header-menu-item" key={link.id}>
+                            <a href={link.pointer}>{link.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     )
 }
