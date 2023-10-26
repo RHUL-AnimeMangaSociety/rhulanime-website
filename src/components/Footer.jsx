15035e134504241d053d3e877dede2cb887d7344
katsofroniou <katerina.sof@btinternet.com>
2023-10-26 04:27:38 +0100

    feat: created footer with credits

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Created by <a href={'https://github.com/katsofroniou'}>Katerina</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
